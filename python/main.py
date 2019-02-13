bbffd5fb5f020cd888372cb03b2dea1a997ffb3c
Takahiro Toriyabe <taka.toriyabe@gmail.com>
2019-02-13 15:17:17 +0900

    Import Shugyo Kozo Kihon Chosa H29

--- python/main.py
+++ python/main.py
@@ -43,18 +43,22 @@ class Main2(FieldMaker, FieldCleaner):
 
         
 if __name__ == '__main__':
-
-    infile = 'C:/Users/Takahiro/Desktop/layout_test.xlsx'
-    index = int(0)
-    outfile = 'C:/Users/Takahiro/Desktop/test'
-    main1 = Main1()
-    main1.run(infile, index, outfile, csv = True)
     
-    infile_base = 'C:/Users/Takahiro/Desktop/h24.xlsx'
-    infile_match = 'C:/Users/Takahiro/Desktop/h19.xlsx'
-    index_base, index_match = 2, 2
+    root_path = 'C:/Users/Takahiro/Desktop/就業構造基本調査'
+
+    infile1 = root_path + '/layout_sheet/（標準記法）平成29年就調個別データL5_2017_RCD_Kobetsu-kk_A.xlsx'
+    infile2 = root_path + '/layout_sheet/H24 【再編成後】個別データレイアウトフォーム.xls'
+    index = 1
+    outfile1 = root_path + '/do-file/H29/H29'
+    outfile2 = root_path + '/do-file/H24/H24'
+    
+    main1 = Main1()
     main2 = Main2()
-    main2.run(infile_base, index_base, infile_match, index_match, outfile)
+    
+    outfile3 = root_path + '/do-file/H29/H29toH24'
+    main1.run(infile1, index, outfile1)
+    main1.run(infile2, index, outfile2)
+    main2.run(infile2, index, infile1, index, outfile3)
 
   
 #    FieldCleaner().CleanField(field1)
