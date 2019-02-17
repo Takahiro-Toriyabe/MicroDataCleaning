7f1d4bb5d2cdd23a47998e854212a409abf28511
Takahiro Toriyabe <taka.toriyabe@gmail.com>
2019-02-17 21:47:51 +0900

    Reorganize DoFileWriter and main interface

--- python/main.py
+++ python/main.py
@@ -4,44 +4,42 @@ from ExcelFileClass import ExcelFile
 from FieldClass import FieldMaker, FieldCleaner
 from VariableCollectorClass import VariableCollector
 from csvFileWriterClass import csvFileWriter
-from DoFileWriterClass import DoFileWriter
-from RenameFileWriterClass import RenameFileWriter
+from DoFileWriterClass import DoFileWriter, RenameFileWriter
 
 
-class Main1(FieldMaker, FieldCleaner):
-
-    def run(self, infile, index, outfile, csv = False):
+class MainBase(FieldMaker, FieldCleaner):
+    
+    def SetSource(self, infile, index, outfile, csv = False):
         field = self.CreateField(ExcelFile(infile, index))
         self.CleanField(field)
         collector = VariableCollector(field)
         if csv == True:
             csv_writer = csvFileWriter(outfile, collector.GetCollection())
             csv_writer.WriteCSVFile()
-        
+            
         collector.CleanCollection()
-        source = collector.GetCollection()
+        return collector.GetCollection()
 
+
+class Main1(MainBase):
+
+    def run(self, infile, index, outfile, csv = False):
+        source = self.SetSource(infile, index, outfile, csv)
         writer = DoFileWriter(outfile, source)
-        writer.WriteAllDoFile()
+        writer.WriteDoFile()
 
         
-class Main2(FieldMaker, FieldCleaner):
+class Main2(MainBase):
     
     def run(self, infile_base, index_base, infile_match, index_match, outfile):
-        field_base = self.CreateField(ExcelFile(infile_base, index_base))
-        self.CleanField(field_base)
-        collector_base = VariableCollector(field_base)
-        Base = collector_base.GetCollection()
+        Base = self.SetSource(infile_base, index_base, outfile)
+        Match = self.SetSource(infile_match, index_match, outfile)
+        source = [Base, Match]
         
-        field_match = self.CreateField(ExcelFile(infile_match, index_match))
-        self.CleanField(field_match)
-        collector_match = VariableCollector(field_match)
-        Match = collector_match.GetCollection()
-        
-        writer = RenameFileWriter()
-        writer.WriteRenameFile(outfile, Base, Match)
+        writer = RenameFileWriter(outfile, source)
+        writer.WriteDoFile()
 
-        
+
 if __name__ == '__main__':
     
     root_path = 'C:/Users/Takahiro/Desktop/就業構造基本調査'
@@ -66,15 +64,3 @@ if __name__ == '__main__':
     main1.run(infile_test, index_test, outfile_test)
 
   
-#    FieldCleaner().CleanField(field1)
-#    FieldCleaner().CleanField(field2)
-#
-#    collector1 = VariableCollector(field1)
-#    collector2 = VariableCollector(field2)
-#    Base = collector1.GetCollection()
-#    Match = collector2.GetCollection()
-#    
-#    outfile = 'C:/Users/Takahiro/Desktop/test_rename.do'
-#    writer = RenameFileWriter()
-#    writer.WriteRenameFile(outfile, Base, Match)
-    
